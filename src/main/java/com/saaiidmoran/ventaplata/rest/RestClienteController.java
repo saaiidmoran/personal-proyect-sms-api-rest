5f7fb58c33a695c9bda1edcde6a480a1071e3044
Said Isaac Jorge Moran <saaiidmoran@gmail.com>
2021-06-16 21:26:32 -0500

    Eliminacion de crossorigin

--- src/main/java/com/saaiidmoran/ventaplata/rest/RestClienteController.java
+++ src/main/java/com/saaiidmoran/ventaplata/rest/RestClienteController.java
@@ -3,7 +3,6 @@ package com.saaiidmoran.ventaplata.rest;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -16,7 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.saaiidmoran.ventaplata.servicedao.ClienteDaoService;
 import com.saaiidmoran.ventaplata.model.Cliente;;
 
-@CrossOrigin(origins = "http://localhost:4200", maxAge = 3600)
+//@CrossOrigin(origins = "http://localhost:4200", maxAge = 3600)
 @RestController
 @RequestMapping("/clienteVPRest")
 public class RestClienteController {
--- src/main/java/com/saaiidmoran/ventaplata/rest/RestDetalleventaController.java
+++ src/main/java/com/saaiidmoran/ventaplata/rest/RestDetalleventaController.java
@@ -3,7 +3,6 @@ package com.saaiidmoran.ventaplata.rest;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -16,7 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.saaiidmoran.ventaplata.model.Detalleventa;
 import com.saaiidmoran.ventaplata.servicedao.DetalleventaDaoService;
 
-@CrossOrigin(origins = "http://localhost:4200", maxAge = 3600)
+//@CrossOrigin(origins = "http://localhost:4200", maxAge = 3600)
 @RestController
 @RequestMapping("/detalleventaVPRest")
 public class RestDetalleventaController {
